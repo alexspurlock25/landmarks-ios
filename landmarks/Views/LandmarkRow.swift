d9f150d73aa3e37c8bb929a4efddfc5805aa34e3
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-10 16:10:29 -0400

    Part 2 - Section 2 done

---
+++ landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  landmarks
+//
+//  Created by Alexander Spurlock on 7/10/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
