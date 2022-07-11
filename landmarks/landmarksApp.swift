ad87a36bafddc8c255b3bdbb05366194ff707775
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-10 20:25:54 -0400

    Part 3 - Section 5 done

--- landmarks/landmarksApp.swift
+++ landmarks/landmarksApp.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 @main
-struct landmarksApp: App {
-    let persistenceController = PersistenceController.shared
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(modelData)
         }
     }
 }
